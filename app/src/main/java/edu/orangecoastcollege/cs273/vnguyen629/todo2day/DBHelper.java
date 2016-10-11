5d023ea3149ad8c352e435aceb1b5d305851cf73
vnguyen629 <brotherflab@gmail.com>
2016-10-11 16:30:38 -0700

    Finished back-end of project

--- app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/todo2day/DBHelper.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/todo2day/DBHelper.java
@@ -1,14 +1,20 @@
 package edu.orangecoastcollege.cs273.vnguyen629.todo2day;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+
+// Not public class DHelper because it contains sensitive information/databases
+// private keyword is not allowed/needed, only need to remove public keyword
 class DBHelper extends SQLiteOpenHelper {
 
     //TASK 1: DEFINE THE DATABASE VERSION, NAME AND TABLE NAME
     private static final String DATABASE_NAME = "ToDo2Day";
-    private static final String DATABASE_TABLE = "Tasks";
+    static final String DATABASE_TABLE = "Tasks";
     private static final int DATABASE_VERSION = 1;
 
 
@@ -37,5 +43,55 @@ class DBHelper extends SQLiteOpenHelper {
                           int newVersion) {
         database.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
         onCreate(database);
+    }
+
+    // Create a method to add a brand new task to the database
+    // Not overwritten, we make them ourselves
+    public void addTask(Task newTask) {
+        // 1.) Create a reference to our database
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // 2.) Make a key-value pair for each value you want to insert
+        ContentValues values = new ContentValues();
+        // Key is name of column, value is the data for the object
+        values.put(KEY_FIELD_ID, newTask.getId());
+        values.put(FIELD_DESCRIPTION, newTask.getDescription());
+        values.put(FIELD_IS_DONE, newTask.getIsDone());
+
+        // 3.) Insert the values into our database
+        db.insert(DATABASE_TABLE, null, values);
+
+        // 4.) Close the database after finished
+        db.close();
+    }
+
+    // Create a method to get/retrieve all the tasks in the database
+    public ArrayList<Task> getAllTasks() {
+        // 1.) Create a reference to the database
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // 2.) Make a new empty ArrayList
+        ArrayList<Task> allTasks = new ArrayList<>();
+
+        // 3.) Query the database for all records (all rows, all fields, and all columns)
+        // The return type of a query is Cursor
+        Cursor results = db.query(DATABASE_TABLE,
+                null, null, null, null, null, null);
+
+        // 4.) Loop through the results and create new Task objects and add them to the ArrayList
+        // If there is no results/nothing in the database, an empty ArrayList is returned
+        if (results.moveToFirst()) {
+            do {
+                int id = results.getInt(0);
+                String description = results.getString(1);
+                int isDone = results.getInt(2);
+                allTasks.add(new Task(id, description, isDone));
+            } while(results.moveToNext());
+        }
+
+        // 5.) Close the database after finished
+        db.close();
+
+        return allTasks;
     }
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/todo2day/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/todo2day/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.orangecoastcollege.cs273.vnguyen629.todo2day;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // FOR NOW (TEMPORARY), delete the old database THEN create the new
+        this.deleteDatabase(DBHelper.DATABASE_TABLE);
+
+        // Make a DBHelper reference
+        DBHelper db = new DBHelper(this);
+
+        // Makes a new task and permanently adds it to the database
+        // The tasks will exist even after the program is closed
+        // Rerunning with adding the same tasks (same ID as before will cause problems)
+        // Thus a temporary measure to avoid this is to delete the database before adding (as done above)
+        db.addTask(new Task(1, "Study for CS273 Midterm", 0));
+        db.addTask(new Task(2, "Play League of Legends", 0));
+        db.addTask(new Task(3, "Register for research symposium", 0));
+        db.addTask(new Task(4, "Learn doubly-linked lists", 0));
+        db.addTask(new Task(5, "Master the FragmentManager", 0));
+
+        // Get all the tasks from the database and print them with Log.i()
+        ArrayList<Task> allTasks = db.getAllTasks();
+        // Loop through each task, print to Log.i()
+        for (Task singleTask : allTasks)
+            Log.i("DATABASE TASK", singleTask.toString());
     }
 }
